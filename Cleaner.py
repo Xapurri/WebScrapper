df15aa2817bc774d0afba01e923a13ba38ecbf21
Xapurri <57631114+Xapurri@users.noreply.github.com>
2020-06-07 19:11:21 +0200

    Add files via upload

---
+++ Cleaner.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 22:47:44 2020
+
+@author: Xavi
+"""
+
+
+from bs4 import BeautifulSoup as bs
+import re, pandas as pd, os
+try:
+    os.remove('csv$.txt')
+    os.remove('data.xlsx')
+    print('Deleted old files\n')
+except:
+    print('Previous file not found')
+    pass
+titulo =[]
+precio = []
+a=1
+error = 0
+escerror = 0
+psa = '\n\n---------------------\n\n'
+# old distritos = ['Eixample','Gràcia','Horta-Guinardó','Les Corts','Nou Barris','Sant Andreu','Sant Martí','Sarria-Sant Gervasi','Sants-Montjuíc','Ciutat Vella']
+distritos = ['Tibidabo','Gracia','Sants-Montjuic','La Barceloneta','El Raval','El Gotic', 'Sant Pere,Santa Caterina','Sant Gervasi-Galvany','El Putxet i el Farro','La Bonanova','Sarria','Les Tres Torres','La Dreta de Eixample','La nova esquerra de Eixample','La Nova Esquerra Eixample','Sagrada Familia','Sant antoni','El Fort Pienc','Horta-Guinardo','Les Corts','Nou Barris','Sant Andreu','Sant MArti']
+
+
+#delimitadores
+delimiters = ('<','>','title=')
+regexPattern = '|'.join(map(re.escape, delimiters))
+pricedelimiter = ('"/','/"')
+regexPricePattern = '|'.join(map(re.escape, pricedelimiter))
+
+with open('csv$.txt','a+') as file:
+    file.write('link$Piso$Precio$Habitaciones$m2$Escalera$Inmobiliaria\n')
+
+url= 'www.idealista.es/'
+
+def auto(distr):
+    titulo = []
+    escerror = 0
+    a=1
+    global error
+    with open(distritos[distr]+'.txt','r',encoding='utf-8') as file:
+        datos=bs(file, features = 'lxml')
+        todo = datos.find_all('div', attrs={"class":"item-info-container"}) 
+        for line in todo:
+            titulo+=[line]
+        for i in range(len(titulo)):
+            try:
+                name = titulo[a].find_all('a', attrs={'class':'item-link'})
+                name2 = re.split(regexPattern, str(name))
+                link = re.split(regexPricePattern, str(name))
+                precio = titulo[a].find_all('span', attrs={'class':'item-price h2-simulated'})
+                precio2 = re.split(regexPattern, str(precio))            
+                tmn = titulo[a].find_all('span', attrs={'class':'item-detail'})
+                tmn2 = re.split(regexPattern, str(tmn))
+                if int(tmn2[2]) > 14:
+                    m2 = tmn2[2]
+                    habitaciones = ' '
+                    escalera = tmn2[10]
+                else:
+                    habitaciones = tmn2[2]
+                    m2 = tmn2[10]
+                    if len(tmn2)<18:
+                        escalera = []
+                        
+                    elif tmn2[18] == ' ':
+                        escalera = tmn2[20]
+                    else:
+                    
+                        escalera = tmn2[18]
+                    
+    
+                try:
+                    inmo = titulo[a].find_all('a', attrs={'data-markup':'listado::logo-agencia'})
+                    inmo = re.split('"',str(inmo))
+                except:
+                    continue
+                a+=1
+                '''with open('datos.txt', 'a+', encoding='utf-8') as docu:
+                    docu.write(str(url+link[1])+'\n')
+                    docu.write(str(name2[3])+'\n')
+                    docu.write((precio2[2])+'\n')
+                    docu.write(str(tmn3)+'\n')
+                    try:
+                        docu.write(str(inmo[5])+'\n\n')
+                        docu.write('----\n\n')
+                    except:
+                        docu.write('\n----\n\n')
+                        continue'''
+                with open('csv$.txt','a+') as csv:
+                    csv.write(str(url+link[1])+'$')
+                    csv.write(str(name2[3])+'$')
+                    csv.write((precio2[2])+'$')
+                    #csv.write(str(tmn2)+'$')
+                    csv.write(str(habitaciones)+'$')
+                    csv.write((m2)+'$')
+                    if escalera != []:
+                        if escalera == ' con ascensor' or escalera == ' sin ascensor' or escalera =='04 abr ':
+                            escalera=''
+                        else:
+                            z = 1
+                        csv.write(str(escalera)+'$')
+                    else:
+                        escalera = ''
+                        csv.write(str(escalera)+'$')
+                        escerror+=1
+    
+                    try:
+                        csv.write(str(inmo[5]+'\n'))
+                        #docu.write('----\n\n')
+                    except:
+                        csv.write('\n')
+                        continue
+                    
+            except:
+                 a+=1
+                 error+=1
+                 print('Error in block ',a,', error num:' ,error,'\n')
+                 pass
+        print(distritos[distr]+' finished!\n')
+
+def excel_maker():
+
+    pd.read_csv('csv$.txt', sep='$', encoding='latin1',dtype='unicode',error_bad_lines=False).to_excel('data.xlsx', sheet_name='sheet1', index=False)
+    file = pd.read_csv('csv$.txt', sep='$', encoding='latin1',dtype='unicode',error_bad_lines=False)
+    print(psa+'Done!\n')
+    print('Rows: '+str(len(file))+'\nTotal Errors: '+str(error)+psa)
+    
+    
+for distr in range(len(distritos)):
+    auto(distr)
+excel_maker()
---
+++ p.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 16:09:48 2020
+
+@author: Xavi
+"""
+
+'Gràcia','Eixample','Gràcia','Horta-Guinardó','Les Corts','Nou Barris','Sant Andreu','Sant Martí','Sarria-Sant Gervasi','Sants-Montjuíc','Ciutat Vella'
+
+
+import pyautogui as gui, pyperclip, time, keyboard
+
+'''
+
+DretaEixample = 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/la-dreta-de-l-eixample/pagina-'
+AntEsqEixample = 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/l-antiga-esquerra-de-l-eixample/pagina-'
+NovEsqEixample = 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/la-nova-esquerra-de-l-eixample/pagina-'
+SagradaFamilia = 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/la-sagrada-familia/pagina-'
+SantAntoni = 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/sant-antoni/pagina-'
+FortPienc = 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/el-fort-pienc/pagina-'
+Gracia = 'https://www.idealista.com/alquiler-viviendas/barcelona/gracia/pagina-'
+HortaGuinardo = 'https://www.idealista.com/alquiler-viviendas/barcelona/horta-guinardo/pagina-'
+LesCorts = 'https://www.idealista.com/alquiler-viviendas/barcelona/les-corts/pagina-'
+NouBarris = 'https://www.idealista.com/alquiler-viviendas/barcelona/nou-barris/pagina-'
+SantAndreu = 'https://www.idealista.com/alquiler-viviendas/barcelona/sant-andreu/pagina-'
+SantMarti = 'https://www.idealista.com/alquiler-viviendas/barcelona/sant-marti/pagina-'
+SGGalvany = 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/sant-gervasi-galvany/pagina-'
+PutxetFarro = 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/el-putxet-i-el-farro/pagina-'
+SGBonanova = 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/sant-gervasi-la-bonanova/pagina-'
+Sarria = 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/sarria/pagina-'
+TresTorres = 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/les-tres-torres/pagina-'
+Tibidabo = 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/vallvidrera-el-tibidabo-i-les-planes/pagina-'
+SantsMontjuic = 'https://www.idealista.com/alquiler-viviendas/barcelona/sants-montjuic/pagina-'
+CiutatVella = 'https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/'
+Raval = 'https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/el-raval/pagina-'
+Gotic = 'https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/el-gotic/pagina-'
+SantaCaterinaIRibera = 'https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/sant-pere-santa-caterina-i-la-ribera/pagina-'
+ciutat-vella/la-barceloneta = https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/la-barceloneta/pagina-
+'''
+
+
+nameDistrito = ['Tibidabo','Gracia','Sants-Montjuic','La Barceloneta','El Raval','El Gotic', 'Sant Pere,Santa Caterina','Sant Gervasi-Galvany','El Putxet i el Farro','La Bonanova','Sarria','Les Tres Torres','La Dreta de Eixample','La nova esquerra de Eixample','La Nova Esquerra Eixample','Sagrada Familia','Sant antoni','El Fort Pienc','Horta-Guinardo','Les Corts','Nou Barris','Sant Andreu','Sant MArti']
+Distritos = ['sarria-sant-gervasi/vallvidrera-el-tibidabo-i-les-planes','Gracia','Sants-Montjuic', 'ciutat-vella/la-barceloneta','ciutat-vella/el-raval', 'ciutat-vella/el-gotic', 'ciutat-vella/sant-pere-santa-caterina-i-la-ribera', 'sarria-sant-gervasi/sant-gervasi-galvany', 'sarria-sant-gervasi/el-putxet-i-el-farro', 'sarria-sant-gervasi/sant-gervasi-la-bonanova', 'sarria-sant-gervasi/Sarria', 'sarria-sant-gervasi/les-tres-torres', 'eixample/la-dreta-de-l-eixample', 'eixample/l-antiga-esquerra-de-l-eixample', 'eixample/la-nova-esquerra-de-l-eixample', 'eixample/la-sagrada-familia', 'eixample/sant-antoni', 'eixample/el-fort-pienc', 'horta-guinardo', 'les-corts', 'nou-barris', 'sant-andreu', 'sant-marti']
+Distritos_links = ['https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/vallvidrera-el-tibidabo-i-les-planes/pagina-','https://www.idealista.com/alquiler-viviendas/barcelona/gracia/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/sants-montjuic/pagina-','https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/la-barceloneta/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/el-raval/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/el-gotic/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/ciutat-vella/sant-pere-santa-caterina-i-la-ribera/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/sant-gervasi-galvany/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/el-putxet-i-el-farro/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/sant-gervasi-la-bonanova/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/sarria/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/sarria-sant-gervasi/les-tres-torres/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/la-dreta-de-l-eixample/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/l-antiga-esquerra-de-l-eixample/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/la-nova-esquerra-de-l-eixample/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/la-sagrada-familia/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/sant-antoni/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/eixample/el-fort-pienc/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/horta-guinardo/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/les-corts/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/nou-barris/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/sant-andreu/pagina-', 'https://www.idealista.com/alquiler-viviendas/barcelona/sant-marti/pagina-']
+finurl = '.htm?ordenado-por=precios-asc'
+
+Distrito_id = 0
+raw_data = ''
+status = 1
+Excepcion_Distritos = ['sarria-sant-gervasi/','eixample/','ciutat-vella/']
+#--------------------#
+
+def auto_txt_save():
+    with open(nameDistrito[dis]+'.txt','a+',encoding="utf-8") as f:   
+      f.write(str(raw_data) + '\n')
+
+def tor_scrapper():
+    global raw_data
+    keyboard.press_and_release('ctrl+shift+i')
+    gui.sleep(0.5)
+    gui.click(1340,341)
+    time.sleep(0.5)
+    gui.click(button='right')
+    time.sleep(0.5)
+    gui.click(1399,577)
+    time.sleep(0.5)
+    gui.click(1679,586)
+    time.sleep(0.5)
+    raw_data = pyperclip.paste()
+    auto_txt_save()
+    keyboard.press_and_release('ctrl+shift+i')
+
+
+
+for dis in range(len(Distritos)):
+    indent=1
+    status = 1
+    row = 0
+    while status == 1:
+        
+        url=Distritos_links[dis]+str(indent)+finurl
+        pyperclip.copy(url)
+        time.sleep(3)
+        #gui.click(1018,1056)
+        time.sleep(3)
+        gui.click(95,195)
+        gui.click(492,65)
+        keyboard.press_and_release('ctrl+v, enter')
+        time.sleep(10)
+        
+        
+        #checks if new url is the same as the one pasted
+
+        gui.click(95,195)   #clica en un sitio irrelevante
+        time.sleep(0.5)
+        gui.click(818,67, button='right')
+        time.sleep(0.5)
+        gui.click(845,126)
+        time.sleep(0.5)
+       
+        check_url = pyperclip.paste()
+
+        if url == check_url:
+            print('Coinciden')
+            
+            status = 1
+        else:
+            lowDistrito = Distritos[dis]
+            new_url =  'https://www.idealista.com/alquiler-viviendas/barcelona/'+ lowDistrito.lower() + '/?ordenado-por=precios-asc'
+            if check_url == new_url and row ==0:
+                print('Primera Página')
+                status = 1
+                row = 1
+
+            else:
+                status = 0
+                print(Distritos[dis]+' finished')
+                break
+        indent+=1    
+        tor_scrapper()
+            
+
+    
+    
+       
+    
+    
+
+
+      
+        
+    
